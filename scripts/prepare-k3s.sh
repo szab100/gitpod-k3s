8eb0206feaaa30366b89f1b19b10e6256d41c655
Szabolcs Fruhwald <git@szab100.com>
2021-10-03 03:00:05 +0000

    use interim base image to build workspaces faster

--- scripts/prepare-k3s.sh
+++ scripts/prepare-k3s.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 script_dirname="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-rootfslock="${script_dirname}/_output/rootfs/rootfs-ready.lock"
-k3sreadylock="${script_dirname}/_output/rootfs/k3s-ready.lock"
+rootfslock="${script_dirname}/rootfs-ready.lock"
+k3sreadylock="${script_dirname}/k3s-ready.lock"
 
 if test -f "${k3sreadylock}"; then
     exit 0
@@ -27,22 +27,24 @@ function waitrootfs() {
   done
 }
 
-echo "🔥 Installing everything, this will be done only one time per workspace."
+echo "🔥 Installing K3S on QEMU, this will run only onnce per workspace."
 
-echo "Waiting for the rootfs to become available, it can take a while, open the terminal #2 for progress"
+echo "Checking if the root filesystem image is available"
 waitrootfs
-echo "✅ rootfs available"
+echo "✅ RootFS found."
 
-echo "Waiting for the ssh server to become available, it can take a while, after this k3s is getting installed"
+echo "Waiting for the VM to start and become reachable by SSH.."
 waitssh
-echo "✅ ssh server available"
+echo "✅ VM has started up."
 
+echo "Installing K3S cluster.."
 ./ssh.sh "curl -sfL https://get.k3s.io | sh -"
 
 mkdir -p ~/.kube
 ./scp.sh root@127.0.0.1:/etc/rancher/k3s/k3s.yaml ~/.kube/config
 
-echo "✅ k3s server is ready"
+echo "✅ K3S cluster is ready, KubeConfig activated."
+echo "Pods running in the cluster:"
 kubectl get pods --all-namespaces
 
 touch "${k3sreadylock}"
--- scripts/qemu.sh
+++ scripts/qemu.sh
@@ -3,10 +3,9 @@
 set -xeuo pipefail
 
 script_dirname="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-outdir="${script_dirname}/_output"
 
 sudo qemu-system-x86_64 -kernel "/boot/vmlinuz" \
--boot c -m 2049M -hda "${outdir}/rootfs/hirsute-server-cloudimg-amd64.img" \
+-boot c -m 2049M -hda "${script_dirname}/imgs/rootfs.img" \
 -net user \
 -smp 8 \
 -append "root=/dev/sda rw console=ttyS0,115200 acpi=off nokaslr" \
