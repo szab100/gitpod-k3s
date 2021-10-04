dfe580dc73a8c778a2d4f2125a72fffb9b4af248
Szabolcs Fruhwald <git@szab100.com>
2021-10-04 01:30:47 +0000

    merge virt-customize cmds into one

--- scripts/prepare-rootfs.sh
+++ scripts/prepare-rootfs.sh
@@ -18,10 +18,6 @@ qemu-img convert -O qcow2 hirsute-server-cloudimg-amd64.img rootfs.img
 rm hirsute-server-cloudimg-amd64.img
 qemu-img resize rootfs.img +20G
 
-sudo virt-customize -a rootfs.img --run-command 'resize2fs /dev/sda'
-
-sudo virt-customize -a rootfs.img --root-password password:root
-
 netconf="
 network:
   version: 2
@@ -31,16 +27,14 @@ network:
       dhcp4: yes
 "
 
-# networking setup
-sudo virt-customize -a rootfs.img --run-command "echo '${netconf}' > /etc/netplan/01-net.yaml"
-
-# copy kernel modules
-sudo virt-customize -a rootfs.img --copy-in /lib/modules/$(uname -r):/lib/modules
-
-# ssh
-sudo virt-customize -a rootfs.img --run-command 'apt remove openssh-server -y && apt install openssh-server -y'
-sudo virt-customize -a rootfs.img --run-command "sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config"
-sudo virt-customize -a rootfs.img --run-command "sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config"
+sudo virt-customize -a rootfs.img \
+--run-command 'resize2fs /dev/sda' \
+--root-password password:root \
+--run-command "echo '${netconf}' > /etc/netplan/01-net.yaml" \
+--copy-in /lib/modules/$(uname -r):/lib/modules \
+--run-command 'apt remove openssh-server -y && apt install openssh-server -y' \
+--run-command "sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config" \
+--run-command "sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config"
 
 sudo rm -rf /var/tmp/.guestfs-0
 echo "rootfs image is ready"
