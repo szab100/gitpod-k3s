2924ab140044b6cb08b8c02f8438916694c11262
Szabolcs Fruhwald <szabolcs_fruhwald@intuit.com>
2021-11-08 11:36:38 -0800

    Revert "test prebuilds"
    This reverts commit a6a08ebac1f70e340c8c9dfb16c1984b9092c1a4.

--- scripts/ws-init.sh
+++ scripts/ws-init.sh
@@ -36,3 +36,9 @@ then
   echo "K3S not found, installer will start shortly.."
   $script_dirname/install-k3s.sh && echo "✅ K3S installation has completed." || fail "❌ K3S installation has failed.. :("
 fi
+
+export KUBECONFIG=/workspace/.kubeconfig
+echo "export KUBECONFIG=/workspace/.kubeconfig" > ~/.bashrc
+echo "⏳ Waiting until the K3S cluster is ready.."
+waitkubectl
+echo "✅ K3S cluster is up, use 'kubectl' or API [:6443] to manage."
--- scripts/ws-start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export KUBECONFIG=/workspace/.kubeconfig
-echo "export KUBECONFIG=/workspace/.kubeconfig" > ~/.bashrc
-echo "⏳ Waiting until the K3S cluster is ready.."
-waitkubectl
-echo "✅ K3S cluster is up, use 'kubectl' or API [:6443] to manage."
